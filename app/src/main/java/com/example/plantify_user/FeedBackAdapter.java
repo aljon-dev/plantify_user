d674f91a6098e536d9f4934446bec9319566ca4f
aljon-dev <140811659+aljon-dev@users.noreply.github.com>
2024-10-01 16:40:28 +0800

    update

---
+++ app/src/main/java/com/example/plantify_user/FeedBackAdapter.java
@@ -0,0 +1,62 @@
+package com.example.plantify_user;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class FeedBackAdapter  extends RecyclerView.Adapter<FeedBackAdapter.ItemHolder> {
+
+    Context context;
+    ArrayList<FeedBackModel>FeedBackList;
+
+
+   public FeedBackAdapter(Context context, ArrayList<FeedBackModel>FeedBackList){
+        this.context = context;
+        this.FeedBackList = FeedBackList;
+    }
+
+
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.feedback_card,parent,false);
+
+        return new ItemHolder(view) ;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+       FeedBackModel feedBackModel = FeedBackList.get(position);
+       holder.onBind(feedBackModel);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return FeedBackList.size();
+    }
+
+    public static class ItemHolder extends RecyclerView.ViewHolder {
+        TextView userEmail,userComment;
+        public ItemHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+            userEmail = itemView.findViewById(R.id.userEmail);
+            userComment = itemView.findViewById(R.id.userComment);
+
+        }
+        public void onBind(FeedBackModel feedBackModel){
+            userEmail.setText(feedBackModel.getEmail());
+            userComment.setText(feedBackModel.getFeedbacks());
+        }
+   }
+}
---
+++ app/src/main/java/com/example/plantify_user/FeedBackModel.java
@@ -0,0 +1,31 @@
+package com.example.plantify_user;
+
+public class FeedBackModel {
+
+    String userid,email,feedbacks;
+
+
+    public String getFeedbacks() {
+        return feedbacks;
+    }
+
+    public void setFeedbacks(String feedbacks) {
+        this.feedbacks = feedbacks;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+}
