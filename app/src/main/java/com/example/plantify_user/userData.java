8f6d12f07f858dce47620a163e9a19ef1d87aaf5
=aljon-dev <aljonjimenez091200@gmail.com>
2024-10-16 02:12:55 +0800

    updated

---
+++ app/src/main/java/com/example/plantify_user/userData.java
@@ -0,0 +1,40 @@
+package com.example.plantify_user;
+
+public class userData {
+
+
+    String Contact,address,username,zipcode;
+
+
+    public String getContact() {
+        return Contact;
+    }
+
+    public void setContact(String contact) {
+        Contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
